63cfd0ea46d1f426f34586696e56018224f54600
tienpn67khmt-tech <tien.pn.67khmt@ntu.edu.vn>
2025-11-16 17:51:20 +0700

    Create toan_tu_gan_phuc_tap.py

---
+++ toan_tu_gan_phuc_tap.py
@@ -0,0 +1,15 @@
+#toantuganphuctap.py
+x=float(input("nhập số x:"))
+print("giá trị ban đầu :",x)
+
+x += 5  
+print("Sau x += 5:", x)      
+
+x *= 2  
+print("Sau x *= 2:", x)      
+
+x //= 4  
+print("Sau x //= 4:", x)     
+
+x **= 2  
+print("Sau x **= 2:", x)     
