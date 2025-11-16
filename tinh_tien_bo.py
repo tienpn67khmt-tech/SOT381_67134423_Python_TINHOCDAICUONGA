d3772c6a9f70e5127c00065c0ba85ce494c68c7a
tienpn67khmt-tech <tien.pn.67khmt@ntu.edu.vn>
2025-11-16 10:37:25 +0700

    Create tinh_tien_bo.py

---
+++ tinh_tien_bo.py
@@ -0,0 +1,9 @@
+x = float(input("nhập tổng hóa đơn (x):"))
+y = 0.15
+z = x * y
+tong = x + z
+print("--- Kết quả Bài 2.1 ---")
+print("Tổng hóa đơn (x):", x)
+print("Phần trăm boa (y):", y)
+print("Tiền boa (z):", z)
+print("Tổng phải thanh toán (tong):", tong)
