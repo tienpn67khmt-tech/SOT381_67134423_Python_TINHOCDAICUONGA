b1ba785aa30e0162be023b1f8fa55bdf35981351
tienpn67khmt-tech <tien.pn.67khmt@ntu.edu.vn>
2025-11-16 16:39:27 +0700

    Create sosanhchuoi.py

---
+++ sosanhchuoi.py
@@ -0,0 +1,7 @@
+#sosanhchuoipy
+str1 = input("Nhập chuỗi thứ nhất (str1): ")
+str2 = input("Nhập chuỗi thứ hai (str2): ")
+print(f"'{str1}' == '{str2}':", str1 == str2)
+print(f"'{str1}' < '{str2}':", str1 < str2)
+print(f"'{str1}' > '{str2}':", str1 > str2)
+print("'abc' < 'abd':", 'abc' < 'abd')
