68814132c0c7c180056ed2719706c621fb3c7dfc
tienpn67khmt-tech <tien.pn.67khmt@ntu.edu.vn>
2025-11-16 21:34:38 +0700

    Create toantumembership.py

---
+++ toantumembership.py
@@ -0,0 +1,8 @@
+#toantumembership.py
+text="hello python"
+subjiect=["Maths","Physics","Chemistry","Biology","English","History"]
+print("'python' in text:", 'python' in text)
+print("'java' not in text:", 'java' not in text)
+
+print("Maths in subject:", 'Maths' in subjiect)
+print("Biology not in subject:", 'Biology' not in subjiect)
