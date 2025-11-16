140d045322331e9a0f0244d8fc61ac3b1ebd5091
tienpn67khmt-tech <tien.pn.67khmt@ntu.edu.vn>
2025-11-16 13:07:29 +0700

    Create sosanhcacsotu.py

---
+++ sosanhcacsotu.py
@@ -0,0 +1,9 @@
+#sosanhsohoc.Py
+x = float(input("Nhập giá trị x:"))
+y = float(input("Nhập giá trị y:"))
+print("x == y:", x == y)  
+print("x != y:", x != y)   
+print("x > y:", x > y)     
+print("x < y:", x < y)    
+print("x >= y:", x >= y)  
+print("x <= y:", x <= y)  
