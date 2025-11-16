5b2096ffa111674723a1e121c34c89cdd9336914
tienpn67khmt-tech <tien.pn.67khmt@ntu.edu.vn>
2025-11-16 23:33:33 +0700

    Create thuc_hanh_toan_tu_gan_phuc_hop.py

---
+++ thuc_hanh_toan_tu_gan_phuc_hop.py
@@ -0,0 +1,19 @@
+#toantuganphuchop.py
+
+total = 100
+print(f"Ban đầu: total = {total}")
+
+total -= 25 
+print(f"Sau khi trừ 25: total = {total}")
+
+total *= 2  
+print(f"Sau khi nhân 2: total = {total}")
+
+total /= 5   
+print(f"Sau khi chia 5: total = {total}")
+
+
+total += 10  
+print(f"Sau khi cộng 10: total = {total}")
+
+print("Kết quả cuối cùng của total là:", total)
