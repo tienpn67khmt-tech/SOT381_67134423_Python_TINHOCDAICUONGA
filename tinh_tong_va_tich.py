aec4d13a90c4bdb34101c67e33413776e18db539
tienpn67khmt-tech <tien.pn.67khmt@ntu.edu.vn>
2025-11-17 08:08:08 +0700

    Create tinh_tong_va_tich.py

---
+++ tinh_tong_va_tich.py
@@ -0,0 +1,11 @@
+#tinh_tong_va_tich.py
+print("Nhập 2 số:")
+x = float(input("Nhập số thứ nhất (x): "))
+y = float(input("Nhập số thứ hai (y): "))
+
+tong = x + y
+
+tich = x * y
+
+print("Tổng của hai số là:", tong)
+print("Tích của hai số là:", tich)
