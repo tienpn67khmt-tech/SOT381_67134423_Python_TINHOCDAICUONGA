9aa8b3b472e0a811aa4beb3d4d888c0f97de529a
tienpn67khmt-tech <tien.pn.67khmt@ntu.edu.vn>
2025-11-23 01:32:14 +0700

    Create kiemtrangaytrongtuan.py

---
+++ kiemtrangaytrongtuan.py
@@ -0,0 +1,20 @@
+#kiemtrangaytrongtuan.py
+n = int(input("Nhập một số (1-7) để xem ngày: "))
+
+if n == 1:
+    print("Hôm nay là: CHỦ NHẬT 🌞")
+elif n == 2:
+    print("Hôm nay là: THỨ HAI 💼")
+elif n == 3:
+    print("Hôm nay là: THỨ BA")
+elif n == 4:
+    print("Hôm nay là: THỨ TƯ")
+elif n == 5:
+    print("Hôm nay là: THỨ NĂM")
+elif n == 6:
+    print("Hôm nay là: THỨ SÁU 🍻")
+elif n == 7:
+    print("Hôm nay là: THỨ BẢY 🏠")
+else:
+    print("Lỗi: Một tuần chỉ có 7 ngày thôi bạn ơi! 😅")
+    
