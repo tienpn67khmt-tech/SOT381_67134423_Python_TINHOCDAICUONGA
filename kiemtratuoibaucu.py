ac0b6cd68722f50b93a97ca009361ba562d2a6df
tienpn67khmt-tech <tien.pn.67khmt@ntu.edu.vn>
2025-11-23 00:16:09 +0700

    Create kiemtratuoibaucu.py

---
+++ kiemtratuoibaucu.py
@@ -0,0 +1,9 @@
+#kiemtratuoibaucu.py
+tuoi = int(input("Hãy nhập tuổi của bạn: "))
+
+if tuoi >= 18:
+  
+    print(f"Bạn {tuoi} tuổi. Bạn đã ĐỦ tuổi trưởng thành! ✅")
+else:
+
+    print(f"Bạn {tuoi} tuổi. Bạn CHƯA đủ tuổi trưởng thành. ❌")
