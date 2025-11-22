9b3066d6f0e564c81840523f09de10a2f2a3b5e3
tienpn67khmt-tech <tien.pn.67khmt@ntu.edu.vn>
2025-11-23 00:18:46 +0700

    Add script to find the larger of two numbers

---
+++ # timsolonnhattronghaichuso.py
@@ -0,0 +1,10 @@
+# timsolonnhattronghaichuso.py
+a = float(input("Nhập số thứ nhất (a): "))
+b = float(input("Nhập số thứ hai (b): "))
+
+if a > b:
+    print(f"Số lớn nhất là a: {a}")
+elif b > a:
+    print(f"Số lớn nhất là b: {b}")
+else:
+    print("Hai số bằng nhau!")
