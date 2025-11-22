224163d270007195fc7bed17446eef9b7241275c
tienpn67khmt-tech <tien.pn.67khmt@ntu.edu.vn>
2025-11-23 00:32:10 +0700

    Create #kiemtradauhayrot.py

---
+++ #kiemtradauhayrot.py
@@ -0,0 +1,7 @@
+#kiemtradauhayrot.py
+diem = float(input("Nhập điểm thi của bạn: "))
+
+if diem >= 5.0:
+    print(f"Điểm của bạn là {diem}. Chúc mừng! Bạn đã ĐẬU. 🎉")
+else:
+    print(f"Điểm của bạn là {diem}. Rất tiếc, bạn đã RỚT. 🌧️")
