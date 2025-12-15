d1a2cf3bd3f52fca22a7203af3e4064e0a1d0910
tienpn67khmt-tech <tien.pn.67khmt@ntu.edu.vn>
2025-12-15 10:07:09 +0700

    Update #kiemtrasohoanhao.py

--- #kiemtrasohoanhao.py
+++ #kiemtrasohoanhao.py
@@ -3,9 +3,9 @@ n = int(input("Nhập số cần kiểm tra (ví dụ: 6): "))
 tong_uoc = 0
 i = 1
 
-while i < n:  # Chỉ duyệt đến n-1 (không tính chính nó)
-    if n % i == 0:  # Nếu i là ước số của n (chia hết)
-        tong_uoc = tong_uoc + i  # Cộng ước số vào tổng
+while i < n: 
+    if n % i == 0:  
+        tong_uoc = tong_uoc + i  
     i += 1
     
 if tong_uoc == n:
