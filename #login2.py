74dd385ceee7d0e89621fb0dac2a04385f662c30
tienpn67khmt-tech <tien.pn.67khmt@ntu.edu.vn>
2025-12-15 15:09:37 +0700

    Add login prompt with input validation

---
+++ #login2.py
@@ -0,0 +1,8 @@
+ky_tu = input("Hãy nhập chữ Y hoặc y: ")
+
+while ky_tu != 'Y' and ky_tu != 'y':
+    print("Bạn nhập sai rồi!")
+    
+    ky_tu = input("Mời nhập lại (Y/y): ")
+    
+print("Login success!")
