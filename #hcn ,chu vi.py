410c2e106b261a67d3fa1970442d3dc30a7bb97c
tienpn67khmt-tech <tien.pn.67khmt@ntu.edu.vn>
2025-12-22 14:15:49 +0700

    Improve input validation for rectangle dimensions
    Refactor rectangle area and perimeter calculation to loop until valid inputs are provided.

--- #hcn ,chu vi.py
+++ #hcn ,chu vi.py
@@ -1,12 +1,18 @@
-w = float(input("Nhập chiều rộng w: "))
-h = float(input("Nhập chiều cao h: "))
+print("Chương trình tính hình chữ nhật (0 <= w, h <= 100)")
 
-if 0 <= w <= 100 and 0 <= h <= 100:
-    chu_vi = (w + h) * 2
-    dien_tich = w * h
+while True:
     
-    print(f"Chu vi: {chu_vi:.2f}")
-    print(f"Diện tích: {dien_tich:.2f}")
-else:
-    print("Vui lòng nhập w và h trong khoảng từ 0.0 đến 100.0")
-            
+    w = float(input("Nhập độ dài cạnh w: "))
+    h = float(input("Nhập độ dài cạnh h: "))
+
+    if 0.0 <= w <= 100.0 and 0.0 <= h <= 100.0:
+        break 
+    else:
+       
+        print("Dữ liệu sai! w và h phải từ 0 đến 100. Nhập lại nhé.")
+
+chu_vi = (w + h) * 2
+dien_tich = w * h
+
+print(f"Chu vi: {chu_vi:.2f}")
+print(f"Diện tích: {dien_tich:.2f}")
